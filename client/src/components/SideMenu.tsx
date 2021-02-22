ab397092c5060329064b01812a9d8c12ddcc07f3
YuiseiMaruyama <yuisei.rock@gmail.com>
2021-02-22 16:31:45 +0900

    update: select部分のsubmitができるように実装

--- client/src/components/SideMenu.tsx
+++ client/src/components/SideMenu.tsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
-import Box from '@material-ui/core/Box'
-import InputLabel from '@material-ui/core/InputLabel'
-import FormControl from '@material-ui/core/FormControl'
-import Select from '@material-ui/core/Select'
-import MenuItem from '@material-ui/core/MenuItem'
+import { Card, CardContent, CardActions, Button, Box, MenuItem } from '@material-ui/core'
 import { useQuery } from '@apollo/client'
 import { DIRECTOR_LIST } from '../query/query'
+import { useForm, Controller } from 'react-hook-form'
+import { Form, Field } from 'react-final-form'
+import { TextField, Select } from 'final-form-material-ui'
+
+type Director = {
+  id: string
+  name: string
+}
+
+type InputForm = {
+  directorName?: string
+  directorAge?: number
+  genre?: string
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      minWidth: 275,
+      minWidth: 500,
     },
     formControl: {
       margin: theme.spacing(1),
@@ -28,61 +33,106 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 )
 
-type Director = {
-  id: string
-  name: string
-}
-
 const SideMenu: React.FC = () => {
   const { data } = useQuery(DIRECTOR_LIST)
+  const { register, trigger, handleSubmit, watch, errors } = useForm<InputForm>()
+  const onSubmit = (value: InputForm) => console.log(value)
   const classes = useStyles()
-  const [director, setDirector] = useState('')
+  const [directorName, setDirector] = useState('')
+  const [directorAge, setAge] = useState('')
+  const [movieName, setMovie] = useState('')
+  const [movieGenre, setGenre] = useState('')
   const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     setDirector(event.target.value as string)
   }
+
   return (
     <div>
-      <Box m={5}>
-        <Card className={classes.root} variant="outlined">
-          <CardContent>
-            <p>映画監督</p>
-            <Box display="flex" flexDirection="column">
-              <TextField label="監督名" />
-              <TextField label="年齢" />
-              <CardActions>
-                <Button variant="outlined" color="primary">
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit, values }) => (
+          <form onSubmit={handleSubmit}>
+            <Card className={classes.root} variant="outlined">
+              <CardContent>
+                <p>映画監督</p>
+                <div>
+                  <Box m={3} display="flex" flexDirection="column">
+                    <Field
+                      name="DirectorName"
+                      component={TextField}
+                      label="監督名"
+                      defaultValue={directorName}
+                      onChange={setDirector}
+                    />
+                    <Field
+                      name="DirectorAge"
+                      component={TextField}
+                      label="年齢"
+                      defaultValue={directorAge}
+                      onChange={setAge}
+                    />
+                  </Box>
+                </div>
+                <Button variant="contained" onChange={handleSubmit} type="submit">
                   追加
                 </Button>
-              </CardActions>
-            </Box>
-          </CardContent>
-        </Card>
-      </Box>
-      <Box m={5}>
-        <Card className={classes.root} variant="outlined">
-          <CardContent>
-            <p>映画作品</p>
-            <Box display="flex" flexDirection="column">
-              <TextField label="作品名" />
-              <TextField label="ジャンル" />
-              <CardActions>
-                <FormControl className={classes.formControl}>
-                  <InputLabel id="director-select-label">Director</InputLabel>
-                  <Select labelId="director-select-label" onChange={handleChange}>
-                    {data &&
-                      data.directorList.map((director: Director) => (
-                        <MenuItem key={director.id}>{director.name}</MenuItem>
-                      ))}
-                  </Select>
-                  <Button variant="outlined" color="primary">
-                    追加
-                  </Button>
-                </FormControl>
-              </CardActions>
-            </Box>
-          </CardContent>
-        </Card>
-      </Box>
+                <pre>{JSON.stringify(values)}</pre>
+              </CardContent>
+            </Card>
+          </form>
+        )}
+      />
+
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit, values }) => (
+          <form onSubmit={handleSubmit}>
+            <Card className={classes.root} variant="outlined">
+              <CardContent>
+                <p>映画作品</p>
+                <div>
+                  <Box m={3} display="flex" flexDirection="column">
+                    <Field
+                      name="MovieName"
+                      component={TextField}
+                      label="作品名"
+                      defaultValue={movieName}
+                      onChange={setMovie}
+                    />
+                    <Field
+                      name="MovieGenre"
+                      component={TextField}
+                      label="ジャンル"
+                      defaultValue={movieGenre}
+                      onChange={setGenre}
+                    />
+                    <Field
+                      fullWidth
+                      name="DirectorName"
+                      component={Select as any}
+                      label="監督選択"
+                      formControlProps={{ fullWidth: true }}
+                      defaultValue={directorName}
+                      onChange={handleChange}
+                    >
+                      {data &&
+                        data.directorList.map((director: Director, key: string) => (
+                          <MenuItem key={key} value={director.id}>
+                            {director.name}
+                          </MenuItem>
+                        ))}
+                    </Field>
+                  </Box>
+                </div>
+                <Button variant="contained" onChange={handleSubmit} type="submit">
+                  追加
+                </Button>
+                <pre>{JSON.stringify(values)}</pre>
+              </CardContent>
+            </Card>
+          </form>
+        )}
+      />
     </div>
   )
 }
